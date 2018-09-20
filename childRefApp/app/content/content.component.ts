8dc1ab070cfa59f2476f0f99cb470edef22abed0
Gogs <gogs@fake.local>
2018-09-20 11:10:58 +0200

    childRefApp corrected a file

--- childRefApp/app/content/content.component.ts
+++ childRefApp/app/content/content.component.ts
@@ -13,35 +13,12 @@ export class ContentComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  // @ContentChildren(RoundComponent)
-  // private round: QueryList<RoundComponent>;
-
-  // @ContentChild("selectMe")
-  // private myElement : ElementRef;
-
-  @ContentChildren("selectMe, meToo")
-  private elements : QueryList<ElementRef>;
-
-  @ContentChildren("another")
-  private elements2 : QueryList<ElementRef>;
-
   ngOnInit() {
   }
 
 
   ngAfterViewInit(): void {
-    window.setTimeout(() => { 
-    this.elements.toArray().forEach(x=>
-    {
-      (<HTMLDivElement>x.nativeElement).classList.add("selected");
-    });
-    this.elements2.toArray().forEach(x=>
-      {
-        (<HTMLDivElement>x.nativeElement).style.color="red";
-      });
-      
 
-    }, 1);
-}
+  }
 
 }
