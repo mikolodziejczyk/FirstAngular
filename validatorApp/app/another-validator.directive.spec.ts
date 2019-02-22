608b87929347466096b143eced18a14d9e35ec13
Gogs <gogs@fake.local>
2019-02-22 09:06:45 +0100

    corrections

--- validatorApp/app/another-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnotherValidatorDirective } from './another-validator.directive';
-
-describe('AnotherValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnotherValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
